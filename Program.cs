8439ec6876271f9ed9cbc87adcbc69d1feefcce5
jose <jmonterrey1993@gmail.com>
2019-04-26 05:09:43 -0400

    terminar proceso en casa

--- Program.cs
+++ Program.cs
@@ -9,7 +9,12 @@ namespace Etapa1
         {
             var escuela = new Escuela("2rey" , 2010, TiposEscuela.Bachillerato
             , pais:"Venezuela", ciudad:"Caracas");
-    
+
+            var curso1 = new Curso();
+            {
+                Nombre = "101";
+                
+            }
             Console.WriteLine(escuela);
         }
     }
