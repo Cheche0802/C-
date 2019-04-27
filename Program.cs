d0077dde5077ef9af8eac83b1e787831d56107d9
jose <jmonterrey1993@gmail.com>
2019-04-26 22:11:40 -0400

    utilizacion de cursos con el metodo for

--- Program.cs
+++ Program.cs
@@ -23,15 +23,16 @@ namespace Etapa1 {
 
             Console.WriteLine (escuela);
             Console.WriteLine ("===============");
-            ImprimirCursosdowhile (arregloCursos);
+            ImprimirCursosfor (arregloCursos);
 
         }
-        private static void ImprimirCursosdowhile (Curso[] arregloCursos) {
-            int contador = 0;
-            do {
-                Console.WriteLine ($"Nombre: {arregloCursos[contador].Nombre} , Id: {arregloCursos[contador].UniqueId}");
-                contador++;
-            } while (contador < arregloCursos.Length);
+        private static void ImprimirCursosfor (Curso[] arregloCursos) {
+            
+            
+            for (int i = 0; i < arregloCursos.Length; i++)
+            {
+              Console.WriteLine ($"Nombre: {arregloCursos[i].Nombre} , Id: {arregloCursos[i].UniqueId}");
+            } 
 
         }
     }
